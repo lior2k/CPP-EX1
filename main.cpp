1cedbbfe253437826aa99e862bf5fa5fc01ea2cf
lior2k <92747945+lior2k@users.noreply.github.com>
2022-03-12 06:07:47 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include "mat.hpp"
+
+int main()
+{
+    char x = 0;
+    int cols, rows;
+    char a, b;
+    while (true) {
+        cout << "Press A to use mat function, press B to exit" << endl;
+        cin >> x;
+        if (x == 'B') {
+            break;
+        }
+        cout << "enter cols" << endl;
+        cin >> cols;
+        cout << "enter rows" << endl;
+        cin >> rows;
+        cout << "enter first char" << endl;
+        cin >> a;
+        cout << "enter second char" << endl;
+        cin >> b;
+        cout << ariel::mat(cols, rows, a, b) << endl;
+    }    
+}
--- mat.cpp
+++ mat.cpp
@@ -1,43 +1,75 @@
 #include "mat.hpp"
 #include <iostream>
-#include <stdexcept>
-#include <string>
+#include <vector>
 using namespace ariel;
 using namespace std;
+const int MAX_CHAR_VAL = 127;
+const int MIN_CHAR_VAL = 32;
 
 string ariel::mat(int cols, int rows, char x, char y) {
-    string result = "";
-    char carpet[rows][cols];
-    char iterations_char;
-    int i = 0;
-
-    // fill matrix
-    while (i < (int)(rows/2)+1) {
-        int j = i;
-        int k = i;
-        if (i % 2 == 0) {
-            iterations_char = x;
-        } else {
-            iterations_char = y;
-        }       
-        // fill rows
-        while (j < cols - i) {
-            carpet[i][j] = iterations_char;
-            carpet[rows - 1 - i][j] = iterations_char;
-            j += 1;
-        }
-        // fill columns
-        while (k < rows - i) {
-            carpet[k][i] = iterations_char;
-            carpet[k][cols - 1 - i] = iterations_char;
-            k += 1;
-        }
-        i += 1;
+    if (cols % 2 == 0 or rows % 2 == 0) {
+        throw std::invalid_argument("invalid arguement - even number");
     }
-    int n, m;
-    for (n = 0; n < rows; n++) {
-        for (m = 0; m < cols; m++) {
-            result += carpet[n][m];
+    if (cols <= 0 or rows <= 0) {
+        throw std::invalid_argument("invalid arguement - 0 or negative number");
+    }
+    if ((int)x >= MAX_CHAR_VAL or (int)x <= MIN_CHAR_VAL or (int)y >= MAX_CHAR_VAL or (int)y <= MIN_CHAR_VAL) {
+        throw std::invalid_argument("invalid arguement - bad char input");
+    }
+    if (cols - (int)cols != 0 or rows - (int)rows != 0) {
+        throw std::invalid_argument("invalid arguement - fraction detected");
+    }
+
+    string result;
+    vector<vector<char>> carpet;
+
+    // initiallize - fill vector with trash values
+    for (int n = 0; n < rows; n++) {
+        vector<char> temp;
+        carpet.push_back(temp);
+        for (int m = 0; m < cols; m++) {
+            carpet[n].push_back('0');     
+        }
+    }
+
+    char toFill = y;
+    int u = 0; // upper row index
+    int d = rows - 1; // lower row index
+    int l = 0; // left column index
+    int r = cols - 1; //right column index
+
+    while (u <= d && l <= r) {
+        toFill = (toFill == y)? x : y;
+
+        // fill rows
+        for (int i = u; i <= r; i++) {
+            // carpet[u].insert(carpet[u].begin() + i, toFill);
+            // carpet[d].insert(carpet[d].begin() + i, toFill);
+            carpet[u][i] = toFill;
+            carpet[d][i] = toFill;
+        }
+
+        // fill columns
+        for (int i = l; i <= d; i++) {
+            // carpet[i].insert(carpet[i].begin() + l, toFill);
+            // carpet[i].insert(carpet[i].begin() + r, toFill);
+            carpet[i][l] = toFill;
+            carpet[i][r] = toFill;
+        }
+        u += 1;
+        l += 1;
+        d -= 1;
+        r -= 1;
+    }
+
+    // build the string
+    for (int n = 0; n < rows; n++) {
+        for (int m = 0; m < cols; m++) {
+            if (result.empty()) {
+                result = carpet[n][m];
+            } else {
+                result += carpet[n][m];
+            }            
         }
         result += '\n';
     }
